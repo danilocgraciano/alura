4464c51355c898e19e8706d73bfb9ce1d6b0cbaf
danilocgraciano <danilocgraciano@gmail.com>
2017-01-17 23:05:00 -0300

    Aula 1
    Fim da Primeira Aula, muito legal!

---
+++ Carreira Desenvolvedor JavaScript/5 Jasmine -Testes Automatizados em Javascript/spec/MaiorEMenorSpec.js
@@ -0,0 +1,35 @@
+describe("Maior e Menor",function(){
+
+	it("Números Sequencias",function(){
+		var algoritmo = new MaiorEMenor();
+		algoritmo.encontra([5,15,7,9]);
+
+		expect(algoritmo.pegaMaior()).toEqual(15);
+		expect(algoritmo.pegaMenor()).toEqual(5);
+	});
+
+	it("Números Ordem Decrescente",function(){
+		var algoritmo = new MaiorEMenor();
+		algoritmo.encontra([9,8,7,6]);
+
+		expect(algoritmo.pegaMaior()).toEqual(9);
+		expect(algoritmo.pegaMenor()).toEqual(6);
+	});
+
+	it("Números Ordem Crescente",function(){
+		var algoritmo = new MaiorEMenor();
+		algoritmo.encontra([1,5,9,10]);
+
+		expect(algoritmo.pegaMaior()).toEqual(10);
+		expect(algoritmo.pegaMenor()).toEqual(1);
+	});
+
+	it("Único número",function(){
+		var algoritmo = new MaiorEMenor();
+		algoritmo.encontra([1]);
+
+		expect(algoritmo.pegaMaior()).toEqual(1);
+		expect(algoritmo.pegaMenor()).toEqual(1);
+	});
+
+});
