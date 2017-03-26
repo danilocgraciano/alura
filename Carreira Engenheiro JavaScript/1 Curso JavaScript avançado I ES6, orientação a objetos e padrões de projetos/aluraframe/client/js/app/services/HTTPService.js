807ded968a4b0ee2d660283eaede8919023b6730
danilocgraciano <danilocgraciano@gmail.com>
2017-03-26 17:20:49 -0300

    Novas Atualizações

--- Carreira Engenheiro JavaScript/1 Curso JavaScript avançado I ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/services/HTTPService.js
+++ Carreira Engenheiro JavaScript/1 Curso JavaScript avançado I ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/services/HTTPService.js
@@ -1,62 +1,78 @@
 class HTTPService {
 
+    static _handleErrors(res) {
+        if (!res.ok)
+            throw new Error(res.statusText);
+        return res;
+    }
+
     static get(url) {
 
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
 
-            /**
-                0: requisição ainda não iniciada
-                1: conexão com o servidor estabelecida
-                2: requisição recebida
-                3: processando requisição
-                4: requisição está concluída e a resposta está pronta
-            */
+        // return new Promise((resolve, reject) => {
+        //     let xhr = new XMLHttpRequest();
+        //     xhr.open('GET', url);
 
-            xhr.onreadystatechange = () => {
+        //     /**
+        //         0: requisição ainda não iniciada
+        //         1: conexão com o servidor estabelecida
+        //         2: requisição recebida
+        //         3: processando requisição
+        //         4: requisição está concluída e a resposta está pronta
+        //     */
 
-                if (xhr.readyState == 4) {
+        //     xhr.onreadystatechange = () => {
 
-                    if (xhr.status == 200) {
+        //         if (xhr.readyState == 4) {
 
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        console.log();
-                        reject(xhr.responseText);
-                    }
+        //             if (xhr.status == 200) {
 
-                }
+        //                 resolve(JSON.parse(xhr.responseText));
+        //             } else {
+        //                 console.log();
+        //                 reject(xhr.responseText);
+        //             }
 
-            };
+        //         }
 
-            xhr.send();
-        });
+        //     };
+
+        //     xhr.send();
+        // });
     }
 
     static post(url, dado) {
 
+        return fetch(url, {
+            headers: { 'Content-type': 'application/json' },
+            method: 'post',
+            body: JSON.stringify(dado)
+        }).then(res => this._handleErrors(res))
+            .then(res => res.json());
 
-        return new Promise((resolve, reject) => {
+        // return new Promise((resolve, reject) => {
 
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = () => {
+        //     let xhr = new XMLHttpRequest();
+        //     xhr.open("POST", url, true);
+        //     xhr.setRequestHeader("Content-type", "application/json");
+        //     xhr.onreadystatechange = () => {
 
-                if (xhr.readyState == 4) {
+        //         if (xhr.readyState == 4) {
 
-                    if (xhr.status == 200) {
+        //             if (xhr.status == 200) {
 
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
+        //                 resolve(JSON.parse(xhr.responseText));
+        //             } else {
 
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send(JSON.stringify(dado));
-        });
+        //                 reject(xhr.responseText);
+        //             }
+        //         }
+        //     };
+        //     xhr.send(JSON.stringify(dado));
+        // });
 
     }
 
