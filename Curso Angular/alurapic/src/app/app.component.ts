506a24efd01008ac6fdec62e858228b17b00f913
Danilo C. Graciano <danilo@compels.net>
2018-07-20 14:35:40 -0300

    Teste git conflito remoto.

--- Curso Angular/alurapic/src/app/app.component.ts
+++ Curso Angular/alurapic/src/app/app.component.ts
@@ -27,10 +27,6 @@ export class AppComponent {
     {
       alt: "compels",
       src: "http://www.compels.net/wp-content/uploads/2018/07/LOGO-DEITADA-PARA-SITE.png"
-    },
-    {
-      alt: "notebook",
-      src: "http://www.compels.net/wp-content/uploads/2016/05/notebook.jpg"
     }
   ];
 }
